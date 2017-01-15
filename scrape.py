60d2ff2b6fe063466cdd88d2255079f2430c305f
bharatkasapoot <bharatkasapoot@yahoo.com>
2017-01-14 20:15:43 -0600

    parsed html to text in new file
    parsed html to new text file text_only.txt
    deleted energy.csv as it is not needed
    cleaned up code to remove csv functions

--- scrape.py
+++ scrape.py
@@ -9,24 +9,17 @@ soup = BeautifulSoup(r.content,'html5lib')
 #print soup.prettify().encode('utf-8')
 #find first instance of table with class name tableAlbumDetail. RETURNS STRING
 tables = soup.find('article', attrs = {'class':'-full'})
-print tables
+#print tables
 filename = 'dump.txt'
 with open(filename,'w') as f:
 	for row in tables.prettify().encode('utf-8'):
 		f.write(row)
+#in above code did not need to add \n as prettify adds that by itself
 #tables contains string of html
 #create a list
-listed = []
-#iterate over all td's which have align = left
-for row in tables.findAll('td', {'align':'left'}):
-	#add each row in tbles using append
-	listed.append(row.text) 
-print listed
 
-
-###print len(listed[::2])
-filename = 'energy.csv'
-with open(filename,'wb') as f:
-	w = csv.writer(f,quoting=csv.QUOTE_ALL)
-	w.writerow(listed[::2])
-
+testing = tables.get_text().encode('utf-8')
+newfilename = 'text_only.txt'
+with open(newfilename,'w') as f:
+	for row in testing:
+		f.write(row)
